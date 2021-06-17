d117300915cef507e8b708ff07161e661b7a0e27
mookkiko <85673159+mookkiko@users.noreply.github.com>
2021-06-17 14:07:05 +0700

    add test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        String fname = args[0];
+        String lname = args[1];
+        Employee Tanyaluk = new Employee(fname,lname); // Default constructor
+        String result = Tanyaluk.getFullName();
+        System.out.println(result);
+    }
+
 }
